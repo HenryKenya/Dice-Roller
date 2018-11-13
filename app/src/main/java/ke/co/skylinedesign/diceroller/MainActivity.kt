92f8b199676e2678ffb88af92066a97934f2eace
HenryKenya <henry.onyango@outlook.com>
2018-11-13 09:49:18 +0300

    Add dice images - Replace text view with dice view image

--- app/src/main/java/ke/co/skylinedesign/diceroller/MainActivity.kt
+++ app/src/main/java/ke/co/skylinedesign/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package ke.co.skylinedesign.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -21,7 +22,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage = findViewById<ImageView>(R.id.dice_image)
+
+        diceImage.setImageResource(drawableResource)
+
     }
 }
